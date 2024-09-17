22d4708cb1c39e7f614d11f67be0d1a488de01d7
Fosco <foscocanc@gmail.com>
2024-09-17 11:11:01 +0200

    Generate `article` resource

--- t-be/src/app.module.ts
+++ t-be/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'sqlite',
       database: 'db.sql',
     }),
+    ArticlesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
