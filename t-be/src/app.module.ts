a50ae32db15fa0b83ffd77ae8601c82b6aa55536
Fosco <foscocanc@gmail.com>
2024-09-17 11:34:12 +0200

    Add entities to module

--- t-be/src/app.module.ts
+++ t-be/src/app.module.ts
@@ -4,17 +4,20 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesModule } from './articles/articles.module';
 import { CategoriesModule } from './categories/categories.module';
+import { Category } from './categories/entities/category.entity';
+import { Article } from './articles/entities/article.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sql',
-    }),
-    ArticlesModule,
-    CategoriesModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'sqlite',
+            database: 'db.sql',
+            entities: [Article, Category],
+        }),
+        ArticlesModule,
+        CategoriesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
