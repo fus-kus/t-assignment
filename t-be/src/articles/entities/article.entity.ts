3978cbdfb9bf33444a3d181d6226a66aed09a793
Fosco <foscocanc@gmail.com>
2024-09-17 11:23:59 +0200

    Add article entity

--- t-be/src/articles/entities/article.entity.ts
+++ t-be/src/articles/entities/article.entity.ts
@@ -1,1 +1,24 @@
-export class Article {}
+import {
+    Column,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Article {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    description: string;
+    @Column()
+    priceAmount: string;
+    @Column()
+    priceCurrency: string;
+    @ManyToMany(() => Category)
+    @JoinTable()
+    categories: Category[];
+}
