da47a36912d5197b16c677ded7b8bad652a4fb55
Fosco <foscocanc@gmail.com>
2024-09-19 11:20:09 +0200

    Add searchbar component

---
+++ t-fe/src/lib/components/ui/searchbar/searchbar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Button from '../button/button.svelte';
+	import Input from '../input/input.svelte';
+</script>
+
+<Input placeholder={'search'} />
+<Button variant={'ghost'} />
