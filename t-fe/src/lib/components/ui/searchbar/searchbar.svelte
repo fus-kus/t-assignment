81830e9e427936eea339db6fed32408707c7e4df
Fosco <foscocanc@gmail.com>
2024-09-19 12:09:55 +0200

    Install component

--- t-fe/src/lib/components/ui/searchbar/searchbar.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Button from '../button/button.svelte';
-	import Input from '../input/input.svelte';
-</script>
-
-<Input placeholder={'search'} />
-<Button variant={'ghost'} />
